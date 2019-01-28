265125b636e8d1ede2bd6144315d3f58f191c7f0
pczhao <pczhao@umich.edu>
2019-01-28 09:34:53 -0500

    Before putting on waves

--- examples/HybridSys/SLIP/run_SLIP_constV_comparison.m
+++ examples/HybridSys/SLIP/run_SLIP_constV_comparison.m
@@ -1,6 +1,6 @@
 % SLIP model with 3 modes.
-% Goal: jump as high as possible for all times
-% running cost = l*cos(theta) [in stance phase], or y [in flight phase]
+% Goal: follow a constant-speed trajectory during t \in [0,T]
+% running cost = ((v*t - 0.5) - x)^2, where v  = 0.1
 % terminal cost = 0
 % 
 clear;
@@ -52,9 +52,9 @@ params.domain{3} = ...
 %-------------------------------------------------------------------------%
 %-------------------------- Parameters for OCP ---------------------------%
 %-------------------------------------------------------------------------%
-d = 4;              % degree of relaxation
-T = 2.5;              % time horizon
-nmodes = 3;         % number of modes
+T = 3;                          % time horizon
+d = 4;                          % degree of relaxation
+nmodes = 3;                     % number of modes
 
 % Solver options
 options.freeFinalTime = 0;      % fixed terminal time
@@ -87,11 +87,11 @@ x{3} = x{2};
 u{3} = u{1};                % dummy variable
 
 % Dynamics
-f{1} = Stance_f_Approx( x{1}, params );
-g{1} = Stance_g_Approx( x{1}, params );
+f{1} = T * Stance_f_Approx( x{1}, params );
+g{1} = T * Stance_g_Approx( x{1}, params );
 for i = 2 : 3
-    f{i} = Flight_f( x{i}, params );
-    g{i} = Flight_g( x{i}, params );
+    f{i} = T * Flight_f( x{i}, params );
+    g{i} = T * Flight_g( x{i}, params );
 end
 
 % Suppports, Reset Maps, and Cost Functions
@@ -101,7 +101,7 @@ l0 = params.l0;
 y = x{1};
 hX{1} = [ domain{1}(:,2) - y;           % domain
           y - domain{1}(:,1) ];
-hU{1} = u{1}*(1 - u{1});
+hU{1} = u{1} * (1 - u{1});
 R{1,2} = Reset_S2F_Approx(y,params);    % reset map
 sX{1,2} = ...                           % guard
         [ -(l0 - y(1))^2;                   % l = l0
@@ -110,29 +110,27 @@ sX{1,2} = ...                           % guard
           domain{2}(:,2) - R{1,2};          % Image(R(i,j)) \subset X_j
           R{1,2} - domain{2}(:,1) ];
 
-h{1} = -y( 1 );                     % h = -l*cos(theta) ~ -l
+h{1} = (0.1*T*t - 0.5 - y(5))^2 * T;       % h = (0.1 * t - 1 - x)^2
 H{1} = 0;
 
 % Mode 2: Flight, y_dot > 0
 y = x{2};
 hX{2} = [ domain{2}(:,2) - y;           % domain
           y - domain{2}(:,1) ];
-hU{2} = u{2}*(1 - u{2});
 R{2,3} = y;                             % reset map
 sX{2,3} = ...                           % guard
         [ -y(4)^2;                          % y_dot = 0
-          hX{3};                            % G \subset X
-          domain{2}(:,2) - R{2,3};          % Image(R(i,j)) \subset X_j
+          hX{2};                            % G \subset X
+          domain{3}(:,2) - R{2,3};          % Image(R(i,j)) \subset X_j
           R{2,3} - domain{3}(:,1) ];
 
-h{2} = -y( 3 );                     % h = -y
+h{2} = (0.1*T*t - 0.5 - y(1))^2 * T;       % h = (0.1 * t - 1 - x)^2
 H{2} = 0;
 
 % Mode 3 : Flight 2
 y = x{3};
 hX{3} = [ domain{3}(:,2) - y;           % domain
           y - domain{3}(:,1) ];
-hU{3} = u{3}*(1 - u{3});
 R{3,1} = Reset_F2S_Approx(y,params);    % reset map
 sX{3,1} = ...                           % guard
         [ -(y(3) - yR)^2;                   % y = yR
@@ -140,11 +138,11 @@ sX{3,1} = ...                           % guard
           domain{1}(:,2) - R{3,1};      	% Image(R(i,j)) \subset X_j
           R{3,1} - domain{1}(:,1) ];
 
-h{3} = -y( 3 );                     % h = -y
+h{3} = (0.1*T*t - 0.5 - y(1))^2 * T;       % h = (0.1 * t - 1 - x)^2
 H{3} = 0;
 
 % Initial condition and Target Set
-x0{3} = [ -1; 0.3; 0.20; 0 ];
+x0{3} = [ -0.5; 0.3; 0.20; 0 ];
 
 % Target set is the entire space
 hXT{1} = hX{1};
@@ -154,7 +152,8 @@ hXT{3} = hX{3};
 %-------------------------------------------------------------------------%
 %-------------------------------- Solve ----------------------------------%
 %-------------------------------------------------------------------------%
-% P = SLIPPlot( 3, x0{3}, params ); 
+P = SLIPPlot( 3, x0{3}, params ); 
+
 
 % function handles
 hf{1} = @(xx) Stance_f_Approx(xx,params);
@@ -169,7 +168,14 @@ R_23 = @(xx) xx;
 R_31 = @(xx) Reset_F2S_Approx( xx, params );
 
 MaxTime = 1;
-seq = [ 3; 1; 2; 3 ];
+switch d
+    case 4
+        seq = [ 3; 1; 2; 3 ];
+    case 6
+        seq = [ 3; 1; 2; 3; 1 ];
+    case 8
+        seq = [ 3; 1; 2; 3; 1 ];
+end
 t_hist = 0;
 state_hist = nan(1,9);
 u_hist = 0;
@@ -205,9 +211,9 @@ for i = 1 : length( seq )
     
 %     if cmode == 1
     [out] = OCPDualSolver( t, xvar, uvar, Tleft*f{cmode}, Tleft*g{cmode}, cx0, chX, chXT, ch, cH, d, options );
-%     end
     pval = pval + Tleft * out.pval;
     total_time = total_time + out.time;
+%     end
     
     
     MaxTime = (T - t_hist(end)) / Tleft;
@@ -215,11 +221,11 @@ for i = 1 : length( seq )
     switch cmode
         case 1          % stance
             controller = @(tt,xx) max(0,min(1,double(subs(out.u{1}, [t;xvar], [tt;xx]))));
-            options = odeset('Events',@(tt,xx) EvtFunc_S2F_scaled(tt,xx,params));
-            options = odeset(options,'AbsTol',1e-9,'RelTol',1e-8);
+            ode_options = odeset('Events',@(tt,xx) EvtFunc_S2F_scaled(tt,xx,params));
+            ode_options = odeset(ode_options,'AbsTol',1e-9,'RelTol',1e-8);
             [ tout, xout, event_time, event_state, event_id ] = ...
                 ode45(@(tt,xx) Tleft * ( hf{1}(xx) + hg{1}(xx) * controller(tt,xx) ), ...
-                             (0 : 1e-2 : MaxTime), cx0, options);
+                             (0 : 1e-2 : MaxTime), cx0, ode_options);
             % Reset
             if ~isempty(event_id)
                 if event_id(end) ~= 1
@@ -230,7 +236,7 @@ for i = 1 : length( seq )
                 end
             end
             % Save
-%             P.Visualize( tout * Tleft + t_hist(end), xout, 1 );
+            P.Visualize( tout * Tleft + t_hist(end), xout, 1 );
             
             t_hist = [ t_hist; t_hist(end) + tout * Tleft ];
             mat = [ eye(5), nan*ones(5,3) ];
@@ -240,11 +246,11 @@ for i = 1 : length( seq )
             
 %             P.Visualize( tout, xout, 1 );
         case 2          % flight 1, ydot > 0
-            options = odeset('Events',@(tt,xx) EvtFunc_F1_scaled(tt,xx,params));
-            options = odeset(options,'AbsTol',1e-9,'RelTol',1e-8);
+            ode_options = odeset('Events',@(tt,xx) EvtFunc_F1_scaled(tt,xx,params));
+            ode_options = odeset(ode_options,'AbsTol',1e-9,'RelTol',1e-8);
             [ tout, xout, event_time, event_state, event_id ] = ...
                 ode45(@(tt,xx) Tleft * ( hf{2}(xx) ), ...
-                             (0 : 1e-2 :  MaxTime), cx0, options);
+                             (0 : 1e-2 :  MaxTime), cx0, ode_options);
             % Reset
             if ~isempty(event_id)
                 if event_id(1) ~= 1
@@ -263,18 +269,18 @@ for i = 1 : length( seq )
 %             end
 %             current_time = tout(end);
             % Plot
-%             P.Visualize( tout * Tleft + t_hist(end), xout, 2 );
+            P.Visualize( tout * Tleft + t_hist(end), xout, 2 );
             t_hist = [t_hist; t_hist(end) + tout * Tleft];
             mat = [ nan*ones(4,4), eye(4) ];
             state_hist = [ state_hist; xout*mat, 2*ones(length(tout),1) ];
             
 %             P.Visualize( tout, xout, 2 );
         case 3          % flight 2, ydot < 0
-            options = odeset('Events',@(tt,xx) EvtFunc_F2S_scaled(tt,xx,params));
-            options = odeset(options,'AbsTol',1e-9,'RelTol',1e-8);
+            ode_options = odeset('Events',@(tt,xx) EvtFunc_F2S_scaled(tt,xx,params));
+            ode_options = odeset(ode_options,'AbsTol',1e-9,'RelTol',1e-8);
             [ tout, xout, event_time, event_state, event_id ] = ...
                 ode45(@(tt,xx) Tleft * ( hf{3}(xx) ), ...
-                             (0 : 1e-2 : MaxTime), cx0, options);
+                             (0 : 1e-2 : MaxTime), cx0, ode_options);
             % Reset
 %             previous_mode = current_mode;
             if ~isempty(event_id)
@@ -289,7 +295,7 @@ for i = 1 : length( seq )
             end
             current_time = tout(end);
             % Plot
-%             P.Visualize( tout * Tleft + t_hist(end), xout, 3 );
+            P.Visualize( tout * Tleft + t_hist(end), xout, 3 );
             
             t_hist = [t_hist; t_hist(end) + tout * Tleft];
             mat = [ nan*ones(4,4), eye(4) ];
@@ -299,38 +305,3 @@ for i = 1 : length( seq )
     end
     
 end
-
-
-
-%% -------------------------------------------------------------------------%
-%-------------------------------- Cost -----------------------------------%
-%-------------------------------------------------------------------------%
-cost = 0;
-for i = 1 : length(t_hist)-1
-    
-    current_mode = state_hist(i,end);
-    
-    if current_mode == 1
-        
-        state = ( state_hist( i, 1:5 ) )';
-        dt = t_hist(i+1) - t_hist(i);
-        cost = cost + dt * double( subs(h{current_mode}, [ t; x{current_mode} ], [ t_hist(i); state ] ) );
-       
-    elseif current_mode > 1
-    
-        state = ( state_hist( i, 5:8 ) )';
-        dt = t_hist(i+1) - t_hist(i);
-        cost = cost + dt * double( subs(h{current_mode}, [ t; x{current_mode} ], [ t_hist(i); state ] ) );
-        
-    else
-        
-        disp('terminate');
-        
-    end
-end
-
-disp(['total time = ', num2str(total_time)]);
-disp(['pval = ', num2str(pval)]);
-disp(['cost = ', num2str(cost)]);
-
-save(['Rebuttal_SLIP_high_d', num2str(d),'T25']);
